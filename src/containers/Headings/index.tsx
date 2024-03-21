70fa7d36dd95555e2fdb9a132ae93106e5324750
KovalchukDanil0 <105009706+KovalchukDanil0@users.noreply.github.com>
2024-03-21 21:30:55 +0100

    improved styles

--- src/containers/Headings/index.tsx
+++ src/containers/Headings/index.tsx
@@ -5,16 +5,11 @@ import { FaArrowRight } from "react-icons/fa";
 import { IHeadings, ISubHeadings } from "../../shared";
 import "./styles.css";
 
-type Props = {
-  headings: IHeadings;
-};
-
-const headingAnimation = "gelatine";
+const headingAnimation = "highlight-headings-gelatine";
 const yOffset = -100;
 
 function ClickHeading(headingsArray: ISubHeadings) {
   if (headingsArray.index >= headingsArray.nodes.length) {
-    headingsArray.nodes[0].classList.remove(headingAnimation);
     headingsArray.index = 0;
   }
 
@@ -22,29 +17,35 @@ function ClickHeading(headingsArray: ISubHeadings) {
   const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
   window.scrollTo({ top: y, behavior: "smooth" });
 
-  if (headingsArray.index !== 0) {
-    headingsArray.nodes[headingsArray.index - 1].classList.remove(
-      headingAnimation,
-    );
-  }
   element.classList.add(headingAnimation);
 
   headingsArray.index++;
 }
 
+type Props = {
+  headings: IHeadings;
+};
+
 export default function Headings(props: Readonly<Props>): React.JSX.Element {
   const iconProps = { color: "black", size: "25px" };
+
   return (
     <Draggable>
       <div className="highlight-headings-headings-popup">
         {Object.keys(props.headings).map((h, idx) => {
-          const nodes = props.headings[h as keyof typeof props.headings];
+          const nodes: ISubHeadings =
+            props.headings[h as keyof typeof props.headings];
+          nodes.nodes.forEach((node) => {
+            node.addEventListener("animationend", function () {
+              node.classList.remove(headingAnimation);
+            });
+          });
+
           return (
             <div
               key={idx}
-              className="highlight-headings-headings-container"
+              className={`highlight-headings-headings-container${nodes.nodes.length === 0 ? " highlight-headings-fade-out" : ""}`}
               style={{
-                opacity: nodes.nodes.length > 0 ? "100%" : "50%",
                 backgroundColor: nodes.color,
               }}
             >
@@ -63,6 +64,7 @@ export default function Headings(props: Readonly<Props>): React.JSX.Element {
               {nodes.nodes.length > 0 ? (
                 <IconContext.Provider value={iconProps}>
                   <FaArrowRight
+                    nodes-heading={h}
                     style={{
                       marginLeft: "15px",
                       cursor: "pointer",
